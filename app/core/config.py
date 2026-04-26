f827c56c3e1f95136ba04751c1a2eb5d678e53b9
rafaldepontes <rafael.cr.carneiro@gmail.com>
2026-04-26 15:28:20 -0300

    chore: cleaning up useless data

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
-    wishlist_path: Path = Path(os.environ.get("WISHLIST_PATH", "./wishlist.json"))
     skins_cache_ttl: int = 3600
     cors_origins: list[str] = ["*"]
     rate_limit: str = "20/minute"
